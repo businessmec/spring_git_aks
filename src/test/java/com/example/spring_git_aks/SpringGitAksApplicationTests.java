f6aec4e2451c571712f01bebdaf4a83b52f3cdc1
businessmec <businessmec@yahoo.com>
2025-10-16 19:09:07 +0200

    create branch remote

---
+++ src/test/java/com/example/spring_git_aks/SpringGitAksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_git_aks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitAksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
