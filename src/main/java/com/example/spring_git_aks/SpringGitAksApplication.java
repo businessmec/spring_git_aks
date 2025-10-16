1c4e0d3eeea9f77b9815f62f60296377e12a2687
businessmec <businessmec@yahoo.com>
2025-10-16 19:38:21 +0200

    commit to branch

--- src/main/java/com/example/spring_git_aks/SpringGitAksApplication.java
+++ src/main/java/com/example/spring_git_aks/SpringGitAksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitAksApplication {
-    String informations = "information";
+    String information = "information";
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGitAksApplication.class, args);
