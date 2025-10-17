03da5cab3c563f735282ac3cd43a109891f9b8d0
businessmec <businessmec@yahoo.com>
2025-10-17 21:29:47 +0200

    tests commit 2 secret variable

--- src/main/java/com/example/spring_git_aks/SpringGitAksApplication.java
+++ src/main/java/com/example/spring_git_aks/SpringGitAksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitAksApplication {
-    String informationj = "information";
+    String information = "information";
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGitAksApplication.class, args);
