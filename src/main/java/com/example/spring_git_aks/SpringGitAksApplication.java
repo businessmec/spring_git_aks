cb246007e5a414fdd6165494b3b09b6bc011baa0
businessmec <businessmec@yahoo.com>
2025-10-17 21:04:55 +0200

    tests commit

--- src/main/java/com/example/spring_git_aks/SpringGitAksApplication.java
+++ src/main/java/com/example/spring_git_aks/SpringGitAksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitAksApplication {
-    String information = "information";
+    String informationj = "information";
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGitAksApplication.class, args);
